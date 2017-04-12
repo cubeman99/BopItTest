0c1a0a9063550396d150fb012387245012d8db2a
Brenden Adamczak <cerebralasylum1@gmail.com>
2017-04-12 13:22:10 -0400

    Update LaunguageTests.cpp
    Fixed all my errors

--- src/LaunguageTests.cpp
+++ src/LaunguageTests.cpp
@@ -11,6 +11,15 @@ TEST(Language, todo)
 	EXPECT_FLOAT_EQ(3.14f, 3.14f);
 }
 
+TEST(Language, todo)
+{
+	EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_NE(0, 1);
+	EXPECT_GT(5, 3);
+	EXPECT_FLOAT_EQ(3.14f, 3.14f);
+}
+
 /*
 the idea i behind these test
 class languageConverter{
@@ -38,37 +47,38 @@ test ifValidAudioChoice
 */
 TEST(Language, checkValid)
 {
-    lg = langugaeConvert()
-	EXPECT_TRUE(false, lg.checkValidChoice("monkey"));
+	lg = langugaeConvert();
+	EXPECT_EQ(false, lg.checkValidChoice("monkey"));
 }
 
 TEST(Language, test _PigLatin)
 {
-    lg = langugaeConvert()
+	lg = langugaeConvert();
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     / examples of translations
     /   http://www.omniglot.com/language/phrases/piglatin.htm
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	EXPECT_TRUE("Oodgay", lg.convertText("pigLatin","Good"));
+	EXPECT_EQ("Oodgay", lg.convertText("pigLatin","Good"));
 }
 
 
 TEST(Language, test_Opish)
 {
-    lg = langugaeConvert()
+	lg = langugaeConvert();
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     / examples of translations
     /   http://www.urbandictionary.com/define.php?term=opish
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	EXPECT_TRUE("ellohop", lg.convertText("opish","hello"));
+	EXPECT_EQ("ellohop", lg.convertText("opish","hello"));
 }
 
 TEST(Language, test_Hex)
 {
-    lg = langugaeConvert()
+	lg = langugaeConvert();
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     / examples of translations
     /   http://codebeautify.org/string-hex-converter
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	EXPECT_TRUE("68656c6c6f", lg.convertText("hex","hello"));
+	EXPECT_EQ("68656c6c6f", lg.convertText("hex","hello"));
 }
+
